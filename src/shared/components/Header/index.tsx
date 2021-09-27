8b98d03e142d443b953379642edd2f0010232b1e
Ana <lidialira@alu.ufc.br>
2021-09-27 19:55:54 -0300

    Add icon navigation

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -4,12 +4,20 @@ import * as S from './styles';
 
 type Props = {
   title: string;
+  isFocused: boolean;
+  action: () => void;
 };
 
-export function Header({title}: Props) {
+export function Header({title, isFocused, action}: Props) {
   return (
     <S.Container>
+      {isFocused && (
+        <S.IconContainer onPress={() => action()}>
+          <S.IconNav name="arrow-left" />
+        </S.IconContainer>
+      )}
       <S.TitlePage>{title}</S.TitlePage>
+      <S.IconContainer />
     </S.Container>
   );
 }
--- src/shared/components/Header/styles.ts
+++ src/shared/components/Header/styles.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components/native';
+import Icon from '~/shared/components/Icon';
+
 import Text from '~/shared/components/Text';
+
+interface IconNavProps {
+  name: string;
+  iconType?: string;
+  iconColor?: string;
+}
 
 export const Container = styled.View`
   width: 100%;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   background-color: ${({theme}) => theme.Colors.COLOR_APPLICATION};
@@ -14,4 +23,23 @@ export const TitlePage = styled(Text).attrs({
   font-weight: bold;
   color: ${({theme}) => theme.Colors.WHITE};
   margin-bottom: 10px;
+  text-align: center;
+`;
+
+export const Touchable = styled.TouchableOpacity``;
+
+export const IconContainer = styled.TouchableOpacity`
+  margin: 0px 0px 0px 10px;
+  position: absolute;
+  left: 10px;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+// top right bottom left
+export const IconNav = styled(Icon).attrs<IconNavProps>(({name, iconType}) => ({
+  name,
+  size: 26,
+  type: iconType,
+}))<IconNavProps>`
+  color: white;
 `;
